eaa5ba1de6b94b7ba7125c6480cc240cf8c795d9
Joyce Almeida <joyce.doti@hotmail.com>
2022-11-27 23:22:20 -0300

    Adding information of Cypress's dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "projectId": "iy9zmm",
     "viewportHeight": 900,
     "viewportWidth": 1440,
     "baseUrl": "https://buger-eats-qa.vercel.app",
